e6255283eb6a129b36f169b49fce993c34560559
Robbani182 <zuhdi.rob@gmail.com>
2026-06-26 18:56:34 +0700

    Hapus semua file

--- routes/console.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Console\AboutCommand;
-
-AboutCommand::add('Service', fn () => ['Name' => 'Service A - Katalog Barang']);
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::view('/graphql-playground', 'graphql-playground');
-
-Route::get('/', function () {
-    return view('welcome');
-});
